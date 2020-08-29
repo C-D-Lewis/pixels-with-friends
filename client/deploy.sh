de5a0344241045d7c50e6f58d8e8abbde0b4e0dd
C-D-Lewis <Bonsitm@gmail.com>
2020-08-29 07:56:32 +0100

    Add CloudFront invalidation to deploy.sh

--- client/deploy.sh
+++ client/deploy.sh
@@ -3,6 +3,7 @@
 set -eu
 
 BUCKET=s3://pixels.chrislewis.me.uk
+CF_DIST_ID=E18GYL9VCU3IK5
 
 echo "Using aws profile $AWS_PROFILE"
 
@@ -17,5 +18,13 @@ aws s3 sync assets $BUCKET/assets
 aws s3 sync styles $BUCKET/styles
 aws s3 cp config.js $BUCKET
 
+# CloudFront invalidation
+RES=$(aws cloudfront create-invalidation --distribution-id $CF_DIST_ID --paths "/*")
+INVALIDATION_ID=$(echo $RES | jq -r '.Invalidation.Id')
+echo "Waiting for invalidation-completed for $INVALIDATION_ID..."
+aws cloudfront wait invalidation-completed --distribution-id $CF_DIST_ID --id $INVALIDATION_ID
+
 # Cleanup
 rm config.js
+
+echo "Deployment complete"
